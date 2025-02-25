f2b78125236b9158a317817ef8517a8ccb7925a7
Phuong Khanh Pham <phuong-khanh.pham@epita.fr>
2025-02-25 23:43:34 +0100

    Added routes

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const { port } = require("./config/env");
-
+const studentRoutes = require("./routes/studentRoutes");
 const app = express();
 app.use(express.json());
 
 connectDB();
 
-app.get("/", (req, res) => {
-  res.send("Node.js API is running");
-});
+
+
+app.use("/api/students", studentRoutes);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
